9d0ce821a226976efe87613988f49bce55105e5d
Ryan Wittman <OfficiallyRy@outlook.com>
2023-02-14 15:42:20 -0700

    Stubbed out controllers, all the routers

---
+++ server/controllers/HousesController.js
@@ -0,0 +1,16 @@
+import BaseController from "../utils/BaseController.js";
+
+
+export class HouseController extends BaseController{
+  constructor(){
+    super('api/houses')
+    this.router
+    .get('',)
+    .get('/:id',)
+    .post('',)
+    .put('/:id',)
+    .delete('/:id',)
+  }
+
+
+}
---
+++ server/controllers/JobsController.js
@@ -0,0 +1,14 @@
+import BaseController from "../utils/BaseController.js"
+
+
+export class JobssController extends BaseController{
+  constructor(){
+    super('api/jobs')
+    this.router
+    .get('', )
+    .get('/:id', )
+    .post('', )
+    .put('/:id', )
+    .delete('/:id', )
+  }
+}
