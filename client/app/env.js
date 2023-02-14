4704071688250ea9d297829d6aacd271151e800b
Ryan Wittman <OfficiallyRy@outlook.com>
2023-02-14 14:28:30 -0700

    put in env stuff and starting my own vers of project

--- client/app/env.js
+++ client/app/env.js
@@ -1,6 +1,6 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
 export const useSockets = false
-export const domain = 'abc-towing.us.auth0.com'
-export const audience = 'https://abc-towing.com'
-export const clientId = 'pECbyD8lsoKeI2mhD1vzWSsIwhZNuO7T'
+export const domain = 'theonetruery.us.auth0.com'
+export const audience = 'https://big-bango-bongo.com/'
+export const clientId = 'akV13tVJ4Z0r96mr8BwmKaFGah40DVxm'
